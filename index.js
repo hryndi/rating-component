c797864072d8e43cb8cde1b0aadfd65f122225c4
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-03-29 00:02:35 +0100

    have done  section after submiting

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const btn = document.querySelector(".btn");
 const modal = document.querySelector(".modal");
+const rateBtns = document.querySelectorAll(".rate-el");
+const thankyouSection = document.querySelector("#thankyou");
+const ratingSection = document.querySelector("#rating");
 
 btn.addEventListener("click", function (e) {
+  ratingSection.style.visibility = "hidden";
+  thankyouSection.style.visibility = "visible";
   modal.classList.add("show");
   setTimeout(function () {
     modal.classList.remove("show");
     clearTimeout();
   }, 5000);
+});
+
+rateBtns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    rateBtns.forEach((el) => {
+      el.classList.remove("selected");
+    });
+    btn.classList.toggle("selected");
+  });
 });
